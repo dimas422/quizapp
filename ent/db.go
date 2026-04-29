367b9b3c6433cfbe73e955f36f91bd771d7c1eef
Dimash <dnauryzbaev2008@gmail.com>
2026-04-29 13:57:45 +0500

    fix ent db outside service

--- ent/db.go
+++ ent/db.go
@@ -1,26 +1,19 @@
 ﻿package ent
 
 import (
-"context"
-"fmt"
+	"database/sql"
+	"fmt"
 
-"entgo.io/ent/dialect"
-entsql "entgo.io/ent/dialect/sql"
-"encore.dev/storage/sqldb"
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-var quizDB = sqldb.Named("quiz")
-
-func OpenEntClient() (*Client, error) {
-db := quizDB.Stdlib()
-drv := entsql.OpenDB(dialect.Postgres, db)
-return NewClient(Driver(drv)), nil
-}
-
-func GetEntClient(ctx context.Context) (*Client, error) {
-client, err := OpenEntClient()
-if err != nil {
-return nil, fmt.Errorf("failed to open ent client: %w", err)
-}
-return client, nil
+func OpenEntClient(dsn string) (*Client, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open db: %w", err)
+	}
+	drv := entsql.OpenDB(dialect.Postgres, db)
+	return NewClient(Driver(drv)), nil
 }
