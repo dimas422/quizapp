54c6370066bc5ae114029d1a70a1ac75337c5cf5
Dimash <dnauryzbaev2008@gmail.com>
2026-04-30 10:27:42 +0500

    fix OpenEntClient - use sqldb properly

--- ent/db.go
+++ ent/db.go
@@ -1,19 +1,17 @@
 ﻿package ent
 
 import (
-	"database/sql"
-	"fmt"
-
-	"entgo.io/ent/dialect"
-	entsql "entgo.io/ent/dialect/sql"
-	_ "github.com/jackc/pgx/v5/stdlib"
+"entgo.io/ent/dialect"
+entsql "entgo.io/ent/dialect/sql"
+"encore.dev/storage/sqldb"
 )
 
-func OpenEntClient(dsn string) (*Client, error) {
-	db, err := sql.Open("pgx", dsn)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open db: %w", err)
-	}
-	drv := entsql.OpenDB(dialect.Postgres, db)
-	return NewClient(Driver(drv)), nil
+var quizDB = sqldb.NewDatabase("quiz", sqldb.DatabaseConfig{
+Migrations: "../auth/migrations",
+})
+
+func OpenEntClient() (*Client, error) {
+db := quizDB.Stdlib()
+drv := entsql.OpenDB(dialect.Postgres, db)
+return NewClient(Driver(drv)), nil
 }
